0b1f1d8838c4bd253b7ae6ddc51224a6e0ba14e4
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2022-11-04 21:47:22 +0200

    style changes to project section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,13 +9,13 @@ import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
 	return (
-		<div id='projects' className='w-full snap-start'>
+		<div id='projects' className='h-screen snap-start'>
 			<div className='max-w-[1240] mx-auto px-2 py-16'>
 				<p className='text-xl tracking-widest uppercase text-[#5651e5]'>
 					Projects
 				</p>
 				<h2 className='py-4'>What I've Built</h2>
-				<div className='grid md:grid-cols-2 gap-8'>
+				<div className='grid md:grid-cols-2 gap-x-20 gap-y-3'>
 					<ProjectItem
 						title='Property Finder'
 						backgroundImg={propertyImg}
