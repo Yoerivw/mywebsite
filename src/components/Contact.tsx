aa0bcdb0fa066999f620da11b654f4cd856b7aa5
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2022-11-07 15:28:33 +0200

    grid styling changes on skills media queries

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 const Contact = () => {
 	return (
-		<div id='contact' className='w-full lg:h-screen snap-start'>
+		<div id='contact' className='w-full h-screen lg:h-screen snap-start'>
 			<div className='max-w-[1240px] m-auto px-2 pt-16 w-full'>
 				<p className='text-xl tracking-widest uppercase text-[#5651e5]'>
 					Contact
@@ -17,21 +17,6 @@ const Contact = () => {
 					{/* left */}
 					<div className='col-span-3 lg:col-span-2 w-full shadow-xl shadow-gray-400 rounded-xl p-4'>
 						<div className='lg:p-4 h-full'>
-							<div>
-								<img
-									className='rounded-xl hover:scale-105 ease-in duration-300'
-									src='https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'
-									alt=''
-								/>
-							</div>
-							<div>
-								<h2 className='py-2'>Yoeri</h2>
-								<p>Front-End Developer</p>
-								<p className='py-4'>
-									I am available for freelance or full-time positions. Contact
-									me and let's talk.
-								</p>
-							</div>
 							<div>
 								<p className='uppercase pt-8'>Connect With Me</p>
 								<div className='flex items-center justify-between py-4'>
@@ -53,54 +38,6 @@ const Contact = () => {
 					</div>
 
 					{/* right */}
-
-					<div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
-						<div className='p-4'>
-							<form>
-								<div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-									<div className='flex flex-col'>
-										<label className='uppercase text-sm py-2'>Name</label>
-										<input
-											className='border-2 rounded-lg p-3 flex border-gray-300'
-											type='text'
-										/>
-									</div>
-									<div className='flex flex-col'>
-										<label className='uppercase text-sm py-2'>
-											Phone Number
-										</label>
-										<input
-											className='border-2 rounded-lg p-3 flex border-gray-300'
-											type='text'
-										/>
-									</div>
-								</div>
-								<div className='flex flex-col py-2'>
-									<label className='uppercase text-sm py-2'>Email</label>
-									<input
-										className='border-2 rounded-lg p-3 flex border-gray-300'
-										type='email'
-									/>
-								</div>
-								<div className='flex flex-col py-2'>
-									<label className='uppercase text-sm py-2'>Subject</label>
-									<input
-										className='border-2 rounded-lg p-3 flex border-gray-300'
-										type='text'
-									/>
-								</div>
-								<div className='flex flex-col py-2'>
-									<label className='uppercase text-sm py-2'>Message</label>
-									<textarea
-										className='border-2 rounded-lg p-3 border-gray-300'
-										rows={5}></textarea>
-								</div>
-								<button className='w-full p-4 text-gray-100 mt-4'>
-									Send Message
-								</button>
-							</form>
-						</div>
-					</div>
 				</div>
 				<div className='flex justify-center py-12'>
 					<Link href='/'>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,13 +3,13 @@ import React from "react";
 
 const Skills = () => {
 	return (
-		<div id='skills' className='w-full lg:h-screen p-2 snap-start'>
+		<div id='skills' className='w-full h-screen p-2 snap-start'>
 			<div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
 				<p className='text-xl tracking-widest uppercase text-[#5651e5'>
 					Skills
 				</p>
 				<h2 className='py-4'>What I can do</h2>
-				<div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+				<div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 md:gap-8'>
 					<div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
@@ -132,7 +132,7 @@ const Skills = () => {
 					</div>
 				</div>
 				<h2 className='py-6'>What I'm tinkering with</h2>
-				<div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+				<div className='grid grid-cols-2 sm:grid-cols-3 gap-2 md:gap-8'>
 					<div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
