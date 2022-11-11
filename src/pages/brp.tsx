bf744692e98d0fcb5ad823637478fe80c5a81f35
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2022-11-11 13:42:44 +0200

    styling change brp

--- src/pages/brp.tsx
+++ src/pages/brp.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const property = () => {
 	return (
-		<div className='w-full'>
+		<div className='w-full overflow-x-hidden'>
 			<div className='w-screen h-[30vh] lg:h-[40vh] relative'>
 				<div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
 				<Image
@@ -29,7 +29,7 @@ const property = () => {
 					</p>
 				</div>
 			</div>
-			<div className='max-w-[1240px] mx-auto px-6 grid md:grid-cols-5 gap-8 pt-8'>
+			<div className='max-w-[1240px] mx-auto px-6 grid md:grid-cols-5 gap-8 py-8'>
 				<div className='col-span-4'>
 					<h2 className='pb-3'>Objectives</h2>
 					<ul className='pl-2'>
@@ -40,15 +40,16 @@ const property = () => {
 							2. Easy navigation to find contact details and location
 						</li>
 						<li className='cursor-text'>
-							3. Easy Access to necessary forms for Clients and Referring
+							3. Easy access to necessary forms for clients and referring
 							doctors
 						</li>
 						<li className='cursor-text'>4. Advertise Careers available</li>
 						<li className='cursor-text'>
-							5. Look at Google Analytics to make meaningful changes to the
-							website
+							5. Use analytics to make meaningful changes to the website
 						</li>
-						<li className='cursor-text'>6. Email Marketing with MailChimp</li>
+						<li className='cursor-text'>
+							6. Simplify routine notifications with email marketing
+						</li>
 					</ul>
 				</div>
 				<div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4'>
@@ -73,9 +74,38 @@ const property = () => {
 						</div>
 					</div>
 				</div>
-				<Link href='/#projects'>
-					<p className='underline cursor-pointer'>Back</p>
-				</Link>
+				<div className='col-span-4 md:col-span-3'>
+					<h2 className='pb-3'>Outcomes</h2>
+					<ul className='pl-2'>
+						<li className='cursor-text'>
+							1. Online booking form which reduced time to appointment by
+							aggregating all information upfront.
+						</li>
+						<li className='cursor-text'>
+							2. 1-click away from necessary information to help with locating
+							or calling a branch
+						</li>
+						<li className='cursor-text'>
+							3. All necessary documents easily downloadable as pdf's from main
+							menu
+						</li>
+						<li className='cursor-text'>
+							4. Notice board above the fold to advertise job openings and
+							events
+						</li>
+						<li className='cursor-text'>
+							5. Used data from analytics in the redesign to move away
+							unnecessary information from primary intent of user.
+							<li className='pl-2 cursor-text'>
+								5.1 Increased Monthly visitors by 1000% over the course of
+								contract
+							</li>
+						</li>
+						<li className='cursor-text'>
+							6. Simplify routine notifications with email marketing
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
