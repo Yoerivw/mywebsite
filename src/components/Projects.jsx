3cd09b8776f787cce97b5f9884d579fb17f87ef3
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2022-11-14 14:26:44 +0200

    shows tools and images dynamically

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import brpImage from "../../public/assets/project-img/brp-image.png";
 import jadeImage from "../../public/assets/project-img/jadetavern-image.png";
 import cwaImage from "../../public/assets/project-img/cwa-screenshot.png";
@@ -7,91 +7,142 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const Projects = () => {
 	const [activeProject, setActiveProject] = useState("brp");
-	const [projectText, setProjectText] = useState({
-		title: "Bergman Ross & partners",
-		text: "Bergman Ross and Partners is a large radiological company.  Their main goal was to create an online booking functionality on their website.  The project included the development the of their entire website, including the booking functionality, as well as the maintenance thereof.",
-		link: "www.bergmanross.co.za",
+	const [projectText, setProjectText] = useState([
+		{
+			title: "Bergman Ross & partners",
+			text: "Bergman Ross and Partners is a large radiological company.  Their main goal was to create an online booking functionality on their website.  The project included the development the of their entire website, including the booking functionality, as well as the maintenance thereof.",
+			link: "www.bergmanross.co.za",
 
-		tools: {
-			wp: { name: "WordPress", imageName: "wordpress" },
-			html: {
-				name: "HTML",
-				imageName: "html",
-			},
-			css: {
-				name: "CSS",
-				imageName: "css",
-			},
-			js: {
-				name: "JS",
-				imageName: "javascript",
-			},
+			tools: [
+				{ name: "WordPress", imageName: "wordpress" },
+				{
+					name: "HTML",
+					imageName: "html",
+				},
+				{
+					name: "CSS",
+					imageName: "css",
+				},
+				{
+					name: "JS",
+					imageName: "javascript",
+				},
+			],
 		},
-	});
+	]);
 	const onJadeClick = async (event) => {
 		setActiveProject("jade");
 
 		await delay(150);
 
-		setProjectText({
-			title: "Jade Tavern",
-			text: "Jade Tavern is a chinese / sushi restaurant in the Southern Suburbs. After the hard lockdown they were in need of an online ordering system. I developed a website for them and integrated a 3rd party tool to handle the ordering functionality.",
-			link: "www.jadetavern.co.za",
-			tools: {
-				wp: { name: "WordPress", imageName: "wordpress" },
-				html: {
-					name: "HTML",
-					imageName: "html",
-				},
-				css: {
-					name: "CSS",
-					imageName: "css",
-				},
-				js: {
-					name: "JS",
-					imageName: "javascript",
-				},
+		setProjectText([
+			{
+				title: "Jade Tavern",
+				text: "Jade Tavern is a chinese / sushi restaurant in the Southern Suburbs. After the hard lockdown they were in need of an online ordering system. I developed a website for them and integrated a 3rd party tool to handle the ordering functionality.",
+				link: "www.jadetavern.co.za",
+				tools: [
+					{ name: "WordPress", imageName: "wordpress" },
+					{
+						name: "HTML",
+						imageName: "html",
+					},
+					{
+						name: "CSS",
+						imageName: "css",
+					},
+					{
+						name: "JS",
+						imageName: "javascript",
+					},
+				],
 			},
-		});
+		]);
 	};
 
 	const onBrpClick = async (event) => {
 		setActiveProject("brp");
 		await delay(150);
 
-		setProjectText({
-			title: "Bergman Ross & partners",
-			text: "Bergman Ross and Partners is a large radiological company.  Their main goal was to create an online booking functionality on their website.  The project included the development the of their entire website, including the booking functionality, as well as the maintenance thereof.",
-			link: "www.bergmanross.co.za",
-			tools: {},
-		});
+		setProjectText([
+			{
+				title: "Bergman Ross & partners",
+				text: "Bergman Ross and Partners is a large radiological company.  Their main goal was to create an online booking functionality on their website.  The project included the development the of their entire website, including the booking functionality, as well as the maintenance thereof.",
+				link: "www.bergmanross.co.za",
+				tools: [
+					{ name: "WordPress", imageName: "wordpress" },
+					{
+						name: "HTML",
+						imageName: "html",
+					},
+					{
+						name: "CSS",
+						imageName: "css",
+					},
+					{
+						name: "JS",
+						imageName: "javascript",
+					},
+				],
+			},
+		]);
 	};
 	const onCwaClick = async (event) => {
 		setActiveProject("cwa");
 		await delay(150);
 
-		setProjectText({
-			title: "CW Attorneys",
-			text: "CW Attorneys is an attorney office specializing in immigration. Their main goal was to enable prospective clients to more easily find contact information and service offerings during lockdown. I built out a design and created a GMB for easy directions.",
-			link: "https://www.chriswentzelattorneys.co.za/",
-			tools: [
-				{
-					html: {
+		setProjectText([
+			{
+				title: "CW Attorneys",
+				text: "CW Attorneys is an attorney office specializing in immigration. Their main goal was to enable prospective clients to more easily find contact information and service offerings during lockdown. I built out a design and created a GMB for easy directions.",
+				link: "https://www.chriswentzelattorneys.co.za/",
+				tools: [
+					{
 						name: "HTML",
 						imageName: "html",
 					},
-					css: {
+					{
 						name: "CSS",
 						imageName: "css",
 					},
-					js: {
+					{
 						name: "JS",
 						imageName: "javascript",
 					},
-				},
-			],
-		});
+				],
+			},
+		]);
 	};
+
+	const tools = projectText.map((project) => {
+		return (
+			<>
+				<h3 className='text-2xl mb-5 b-5 transition-transform underline'>
+					{project.title}
+				</h3>
+				<p className='transition-opacity max-w-[550px] pb-8 '>{project.text}</p>
+				<p className=' font-bold'>Tools Used:</p>
+				<div className='flex flex-row space-x-12 mt-4 mb-8 border-l-2 border-l-gray-300 pl-2'>
+					{project.tools.map((tool) => {
+						const url = `/../public/assets/skill-icons/${tool.imageName}.png`;
+						return (
+							<>
+								<div className='text-center'>
+									<Image src={url} width={32} height={32} alt='/' />
+
+									<p>{tool.name}</p>
+								</div>
+							</>
+						);
+					})}
+				</div>
+				<a
+					href={project.link}
+					className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+					View Site
+				</a>
+			</>
+		);
+	});
 
 	return (
 		<div
@@ -135,62 +186,7 @@ const Projects = () => {
 							</li>
 						</ul>
 					</div>
-					<div>
-						<h3 className='text-2xl mb-5 b-5 transition-transform underline'>
-							{projectText.title}
-						</h3>
-						<p className='transition-opacity max-w-[550px] pb-8 '>
-							{projectText.text}
-						</p>
-						<p className=' font-bold'>Tools Used:</p>
-						<div className='flex flex-row space-x-12 mt-4 mb-8 border-l-2 border-l-gray-300 pl-2'>
-							<div className='text-center'>
-								<Image
-									src={"/../public/assets/skill-icons/wordpress.png"}
-									width={32}
-									height={32}
-									alt='/'
-								/>
-
-								<p>WordPress</p>
-							</div>
-							<div className='text-center'>
-								<Image
-									src={"/../public/assets/skill-icons/html.png"}
-									width={32}
-									height={32}
-									alt='/'
-								/>
-
-								<p>HTML</p>
-							</div>
-							<div className='text-center'>
-								<Image
-									src={"/../public/assets/skill-icons/css.png"}
-									width={32}
-									height={32}
-									alt='/'
-								/>
-
-								<p>CSS</p>
-							</div>
-							<div className='text-center'>
-								<Image
-									src={"/../public/assets/skill-icons/javascript.png"}
-									width={32}
-									height={32}
-									alt='/'
-								/>
-
-								<p>JS</p>
-							</div>
-						</div>
-						<a
-							href={`"${projectText.link}"`}
-							className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-							View Site
-						</a>
-					</div>
+					<div>{tools}</div>
 				</div>
 			</div>
 		</div>
