9103e64ae3795d19da0ce52ca78868a0aa6d2a3b
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2022-11-14 13:26:29 +0200

    added tool object to state

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,22 @@ const Projects = () => {
 		title: "Bergman Ross & partners",
 		text: "Bergman Ross and Partners is a large radiological company.  Their main goal was to create an online booking functionality on their website.  The project included the development the of their entire website, including the booking functionality, as well as the maintenance thereof.",
 		link: "www.bergmanross.co.za",
-		image: brpImage,
+
+		tools: {
+			wp: { name: "WordPress", imageName: "wordpress" },
+			html: {
+				name: "HTML",
+				imageName: "html",
+			},
+			css: {
+				name: "CSS",
+				imageName: "css",
+			},
+			js: {
+				name: "JS",
+				imageName: "javascript",
+			},
+		},
 	});
 	const onJadeClick = async (event) => {
 		setActiveProject("jade");
@@ -20,9 +35,23 @@ const Projects = () => {
 
 		setProjectText({
 			title: "Jade Tavern",
-			text: "Bergman Ross and Partners is a large radiological company. Their main goal was to create an online booking functionality on their website. The project included the development the of their entire website, including the booking functionality, as well as the maintenance thereof.",
+			text: "Jade Tavern is a chinese / sushi restaurant in the Southern Suburbs. After the hard lockdown they were in need of an online ordering system. I developed a website for them and integrated a 3rd party tool to handle the ordering functionality.",
 			link: "www.jadetavern.co.za",
-			image: jadeImage,
+			tools: {
+				wp: { name: "WordPress", imageName: "wordpress" },
+				html: {
+					name: "HTML",
+					imageName: "html",
+				},
+				css: {
+					name: "CSS",
+					imageName: "css",
+				},
+				js: {
+					name: "JS",
+					imageName: "javascript",
+				},
+			},
 		});
 	};
 
@@ -34,7 +63,7 @@ const Projects = () => {
 			title: "Bergman Ross & partners",
 			text: "Bergman Ross and Partners is a large radiological company.  Their main goal was to create an online booking functionality on their website.  The project included the development the of their entire website, including the booking functionality, as well as the maintenance thereof.",
 			link: "www.bergmanross.co.za",
-			image: brpImage,
+			tools: {},
 		});
 	};
 	const onCwaClick = async (event) => {
@@ -43,21 +72,36 @@ const Projects = () => {
 
 		setProjectText({
 			title: "CW Attorneys",
-			text: "Bergman Ross and Partners is a large radiological company. Their main goal was to create an online booking functionality on their website. The project included the development the of their entire website, including the booking functionality, as well as the maintenance thereof.",
+			text: "CW Attorneys is an attorney office specializing in immigration. Their main goal was to enable prospective clients to more easily find contact information and service offerings during lockdown. I built out a design and created a GMB for easy directions.",
 			link: "https://www.chriswentzelattorneys.co.za/",
-			image: cwaImage,
+			tools: [
+				{
+					html: {
+						name: "HTML",
+						imageName: "html",
+					},
+					css: {
+						name: "CSS",
+						imageName: "css",
+					},
+					js: {
+						name: "JS",
+						imageName: "javascript",
+					},
+				},
+			],
 		});
 	};
 
 	return (
 		<div
 			id='projects'
-			className='w-full md:h-screen snap-start flex items-center text-gray-700'>
+			className='w-full h-screen snap-start flex items-center text-gray-700'>
 			<div className='max-w-[1240px] w-screen mx-auto px-2 py-14'>
 				<h2 className='mb-10'>Websites I've built</h2>
 
 				<div className='flex flex-column flex-wrap justify-between relative'>
-					<div className="py-[5px] pr-[30px] before:content-[' '] before:w-[60px] before:h-[60px] before:border-r-black before:border-r-2 before:border-t-black before:border-t-2 before:absolute before:left-[130px]">
+					<div className="py-[5px] pr-[30px] before:content-[' '] before:w-[60px] before:h-[60px] before:border-r-black before:border-r-2 before:border-t-black before:border-t-2 before:absolute before:left-[130px] before:top-[-2px]">
 						<ul className='text-lg leading-loose min-w-[160px]'>
 							<li
 								id='brp'
