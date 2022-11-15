657680cdc56e3c224955b0d7e69b8de738b2d78d
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2022-11-15 10:55:22 +0200

    removed unused links from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,9 +51,7 @@ const Navbar = () => {
 						<Link href='/#home'>
 							<li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
 						</Link>
-						<Link href='/#about'>
-							<li className='ml-10 text-sm uppercase hover:border-b'>About</li>
-						</Link>
+
 						<Link href='/#projects'>
 							<li className='ml-10 text-sm uppercase hover:border-b'>
 								Projects
@@ -61,11 +59,6 @@ const Navbar = () => {
 						</Link>
 						<Link href='/#skills'>
 							<li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
-						</Link>
-						<Link href='/#contact'>
-							<li className='ml-10 text-sm uppercase hover:border-b'>
-								Contact
-							</li>
 						</Link>
 					</ul>
 					<div onClick={handleNav} className='md:hidden'>
@@ -105,11 +98,7 @@ const Navbar = () => {
 									Home
 								</li>
 							</Link>
-							<Link href='/#about'>
-								<li onClick={() => setNav(false)} className='py-4 text-sm'>
-									About
-								</li>
-							</Link>
+
 							<Link href='/#projects'>
 								<li onClick={() => setNav(false)} className='py-4 text-sm'>
 									Projects
@@ -118,16 +107,6 @@ const Navbar = () => {
 							<Link href='/#skills'>
 								<li onClick={() => setNav(false)} className='py-4 text-sm'>
 									Skills
-								</li>
-							</Link>
-							<Link href='/#contact'>
-								<li onClick={() => setNav(false)} className='py-4 text-sm'>
-									Contact
-								</li>
-							</Link>
-							<Link href='/'>
-								<li onClick={() => setNav(false)} className='py-4 text-sm'>
-									Blog
 								</li>
 							</Link>
 						</ul>
